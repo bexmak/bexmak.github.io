3dd8ea4de2f42723d71a3a20480763a1e5072474
bexmak <Bex@Bex.local>
2016-08-30 21:29:55 +0800

    Bex Bootstrap
    Aug 2016

---
+++ bexbootstrap/js/insta.js
@@ -0,0 +1,10 @@
+
+<script type="text/javascript">
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'instameethk',
+        clientId: '49feb347a44649aba8a9bf3b6efc7a4c'
+    });
+    feed.run();
+</script>
+      
