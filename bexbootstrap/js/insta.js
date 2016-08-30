1818cd9d8de94e6e79a77988279ab05d251a5717
bexmak <Bex@Bex.local>
2016-08-30 22:02:55 +0800

    only my account js

--- bexbootstrap/js/insta.js
+++ bexbootstrap/js/insta.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 	    
 	    $jQuery('.feed').spectragram('getUserFeed',{
 	      max: 12,
-	      query: 'cats',
+	      query: 'bexmak',
 	      wrapEachWith: '<div class="photo">'
 	    });
 	  }
