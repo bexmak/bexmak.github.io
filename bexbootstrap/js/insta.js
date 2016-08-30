74dd787ec18fa5c1c417284d4b9fb85a57b4f510
bexmak <Bex@Bex.local>
2016-08-30 21:56:11 +0800

    js on load only

--- bexbootstrap/js/insta.js
+++ bexbootstrap/js/insta.js
@@ -1,19 +1,22 @@
-var Spectra = {
-  instaToken: '34325439.49feb34.d0045066f4bd4849b10d40e5b184facc',
-  instaID: '49feb347a44649aba8a9bf3b6efc7a4c',
-  
-  init: function () {
-    $.fn.spectragram.accessData = {
-      accessToken: this.instaToken,
-      clientID: this.instaID
-    };
-    
-    $('.feed').spectragram('getUserFeed',{
-      max: 12,
-      query: 'cats',
-      wrapEachWith: '<div class="photo">'
-    });
-  }
-}
+$(document).ready(function () {
+	var Spectra = {
+	  instaToken: '34325439.49feb34.d0045066f4bd4849b10d40e5b184facc',
+	  instaID: '49feb347a44649aba8a9bf3b6efc7a4c',
+	  
+	  init: function () {
+	    $jQuery.fn.spectragram.accessData = {
+	      accessToken: this.instaToken,
+	      clientID: this.instaID
+	    };
+	    
+	    $jQuery('.feed').spectragram('getUserFeed',{
+	      max: 12,
+	      query: 'cats',
+	      wrapEachWith: '<div class="photo">'
+	    });
+	  }
+	}
 
-Spectra.init();
+	Spectra.init();
+
+})
