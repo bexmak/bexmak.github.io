67b1b31ef921471b4f207d2f84684683e9564696
bexmak <Bex@Bex.local>
2016-08-30 21:52:37 +0800

    modified js for spectra

--- bexbootstrap/js/insta.js
+++ bexbootstrap/js/insta.js
@@ -1,6 +1,19 @@
+var Spectra = {
+  instaToken: '34325439.49feb34.d0045066f4bd4849b10d40e5b184facc',
+  instaID: '49feb347a44649aba8a9bf3b6efc7a4c',
+  
+  init: function () {
+    $.fn.spectragram.accessData = {
+      accessToken: this.instaToken,
+      clientID: this.instaID
+    };
+    
+    $('.feed').spectragram('getUserFeed',{
+      max: 12,
+      query: 'cats',
+      wrapEachWith: '<div class="photo">'
+    });
+  }
+}
 
-jQuery.fn.spectragram.accessData = {
-    accessToken: '34325439.49feb34.d0045066f4bd4849b10d40e5b184facc',
-    clientID: '49feb347a44649aba8a9bf3b6efc7a4c'
-};
-
+Spectra.init();
