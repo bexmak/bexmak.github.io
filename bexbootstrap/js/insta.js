b009e2c0ab3f01817e4dfe156c9603341fc7f9a5
bexmak <Bex@Bex.local>
2016-08-30 21:38:45 +0800

    4

--- bexbootstrap/js/insta.js
+++ bexbootstrap/js/insta.js
@@ -1,6 +1,6 @@
 
 jQuery.fn.spectragram.accessData = {
-    accessToken: '[your-instagram-access-token]',
+    accessToken: '34325439.49feb34.d0045066f4bd4849b10d40e5b184facc',
     clientID: '49feb347a44649aba8a9bf3b6efc7a4c'
 };
 
